86c1b4e2ee954af04c1119d3c0cdf7c696ae8bf4
zhuty <13718054285@163.com>
2021-07-22 19:25:41 -0700

    最好别改了

--- all.py
+++ all.py
@@ -2,19 +2,7 @@ import os
 # 比较内核态进程列表与ps命令，找到隐藏进程
 # 可以解决内核态劫持的情况
 
-os.system("sudo bash scan.sh")
-p = []
-with open("1.log", 'r', encoding="utf8") as f:
-    k = False
-    for line in f:
-        if line.__contains__("进程扫描模块开始运行"):
-            k = True
-        elif line.__contains__("进程扫描模块卸载成功"):
-            pass
-        elif k:
-            t = line.split()
-            p.append((t[-2], t[-1]))
-
+# 读取ps命令输出
 cmd = "ps -e"
 process = os.popen(cmd)
 output = process.read()
@@ -26,18 +14,27 @@ k = []
 for i in range(1, t - 2):
     k.append((output[i * 4], output[i * 4 + 3]))
 
-hiding = []
-
-
-def legal_kworker(n):
-    for i in range(0, len(k)):
-        if k[i][0] == n[0] and k[i][1].startswith(n[1]):
-            return True
-    return False
-
-
-# 后四行是装载模块读取进程列表时的sh, sudo, bash, insmod命令
-filter = ['insmod', 'bash', 'sudo', 'sh']
+# 在内核态遍历进程
+os.system("sudo insmod scan.ko")
+os.system("sudo rmmod scan")
+cmd = "dmesg -c"
+process = os.popen(cmd)
+output = process.read()
+process.close()
+f = output.split('\n')
+f.remove('')
+p = []
+h = False
+for line in f:
+    if line.__contains__("进程扫描模块开始运行"):
+        h = True
+    elif line.__contains__("进程扫描模块卸载成功"):
+        pass
+    elif h:
+        t = line.split()
+        p.append((t[-2], t[-1]))
+# 末尾有装载模块读取进程列表时的sh, sudo, insmod命令，需要过滤掉
+filter = ['insmod', 'sudo', 'sh']
 fi = {}
 for i in range(-1, -len(p), -1):
     if p[i][1] in filter:
@@ -46,10 +43,32 @@ for i in range(-1, -len(p), -1):
         if len(filter) == 0:
             break
 
+# 检测隐藏进程
+hiding = []
+
+
+# 因为内核态和ps命令的kworker进程输出的进程名不同，进行核查
+def legal_kworker(n):
+    for i in range(0, len(k)):
+        if k[i][0] == n[0] and k[i][1].startswith(n[1]):
+            return True
+    return False
+
+
 for i in range(0, len(p)):
     if not (((p[i] in k) or fi.get(i, False)) or legal_kworker(p[i])):
         print(p[i])
         hiding.append(p[i])
+
+# 检查是否为误差
+for i in hiding:
+    cmd = "ps -e | grep \"" + i[1] + "\""
+    process = os.popen(cmd)
+    output = process.read()
+    process.close()
+    output = output.split()
+    if len(output) == 1:
+        hiding.remove(i)
 
 if hiding == []:
     print("当前没有隐藏的进程！")
